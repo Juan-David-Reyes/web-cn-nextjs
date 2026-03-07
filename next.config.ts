a8a61305bca4116e60f2d591b0269daad7d3e365
Juan David <jdavidreyes09@gmail.com>
2026-03-07 18:06:24 -0500

    fix images blog

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'pdf.ms.credential.net',
       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '3000',
+      }
     ],
   },
 };
